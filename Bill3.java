2583bfde5b10aeeef61f25f6e9298ed2bef60ce9
danakoren <95614099+danakoren@users.noreply.github.com>
2024-11-10 11:49:58 +0200

    Update Bill3.java

--- Bill3.java
+++ Bill3.java
@@ -2,7 +2,16 @@
 public class Bill3 {
 	public static void main(String[] args) {
 		// To get you started, here is the first line in the program:
-	    String name1 = args[0];
+	    String name1 = (args[0]);
+		String name2 = (args[1]);
+		String name3 = (args[2]);
+		int bill = Integer.parseInt(args[3]);
+		double s = Math.ceil((double)bill/3);
+
+		System.out.println("Dear " + name1 + " " + name2 + " " + name3 + " : pay " + s  );
+
+
+
 	    // Replace this comment with the rest of your code   
 	}
 }
