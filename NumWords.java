d7bbe8c223af7e5e4e6e43b5f425bafeb70e5e78
danakoren <dana.koren@post.runi.ac.il>
2024-11-14 13:16:17 +0200

    8 try

--- NumWords.java
+++ NumWords.java
@@ -3,7 +3,7 @@ public class NumWords {
 	public static void main(String args[]) {
 	    // Replace this comment with your code
 		int num = Integer.parseInt(args[0]);
-		System.out.print(Math.floorMod(num / 100, 10) + " hundreds, ");
+		System.out.print(num / 100 + " hundreds, ");
 		System.out.print(Math.floorMod(num / 10, 10) + " tens, ");
 		System.out.print("and " + Math.floorMod(num, 10) + " ones.");
 //System.out.println(hundreds);
